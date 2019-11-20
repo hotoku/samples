670699b0cd8703a3320b4afa98f1a78dcbf78e1e
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2019-11-21 08:18:43 +0900

    docker終了時にコンテナを削除

--- r/run.sh
+++ r/run.sh
@@ -1,4 +1,4 @@
 #/bin/bash 
 
 DOCKER=/usr/local/bin/docker
-${DOCKER} run -d -e PASSWORD=hotoku -v /Users/hotoku/sample/r:/home/rstudio -p 8787:8787 hotoku/r
+${DOCKER} run -d --rm -e PASSWORD=hotoku -v /Users/hotoku/sample/r:/home/rstudio -p 8787:8787 hotoku/r
