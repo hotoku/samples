ef11658d44e6a324fffb22273b1e05823ee57062
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2019-11-27 19:13:38 +0900

    WriteToBigQueryに変更
    ソースにBigQuerySinkは Deprecatedとの記載あり

---
+++ beam/4/run3.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import apache_beam as beam
+
+
+def myprint(l):
+    print(l)
+    return l
+
+project = "astute-cumulus-230103"
+p = beam.Pipeline(argv=[
+    f"--project={project}"
+])
+
+(p
+ | beam.io.Read(beam.io.BigQuerySource(
+     project=project,
+     query="select * from `horikoshi_work.temp_20191127_stayhome_sample`",
+     use_standard_sql=True))
+ | beam.Map(myprint)
+ | beam.io.WriteToBigQuery(
+     table="temp_20191127_3",
+     dataset="horikoshi_work",
+     project=project,
+     schema=",".join(["uuid:STRING",
+                      "latitude:FLOAT",
+                      "longitude:FLOAT",
+                      "accuracy:FLOAT",
+                      "timestamp:INTEGER",
+                      "datetime_jst:DATETIME"]))
+)
+ 
+p.run().wait_until_finish()
