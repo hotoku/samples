94b95b8764bb033716374ea6903ccb7cc5ab71f1
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2019-11-28 23:28:08 +0900

    rstan: 階層ベイズのサンプルコード

--- r/rstan/2/2_run.r
+++ r/rstan/2/2_run.r
@@ -1,10 +1,13 @@
 library(rstan)
+library(tidyverse)
+
+set.seed(101)
 
 ITER <- 5e4
 WARMUP <- 1e4
 CHAINS <- 4
 
-NUM_DATA <- 300
+NUM_DATA <- 1000
 NUM_CLASS <- 3
 
 CLASS <- sample(1:NUM_CLASS, NUM_DATA, replace = TRUE)
@@ -28,3 +31,5 @@ model <- stan_model(file="rstan/2/2_model.stan")
 stan_fit <- sampling(model, data=dat, verbose=TRUE,
                      iter = ITER, warmup = WARMUP, chains = CHAINS,
                      cores = 4)
+
+save.image(file="2.RData")
