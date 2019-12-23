9e90bcaf76120f239979001cc91626c763982f16
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2019-12-23 09:12:55 +0900

    [r] docopt

---
+++ r/docopt/1.r
@@ -0,0 +1,20 @@
+library(docopt)
+
+"Usage: docopt.r [-h] [--opt1 OPT1] ARG1 ARG2
+
+-h --help   show this
+--opt1 OPT1 option 1
+ARG1        arg 1
+ARG2        arg 2 
+" -> doc
+
+
+docopt(doc, "--opt1 XXX a b")
+
+docopt(doc, "XXX YYY") # エラー
+# opt1がオプショナルになってない..
+
+# 実際の引数をparseするときはこっち
+# docopt(doc) 
+
+
