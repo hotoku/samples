ffb5e47dfd7de1b8e66fa3886c7233640217896c
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-01-05 18:21:16 +0900

    [r] logging

---
+++ r/logging/1.r
@@ -0,0 +1,6 @@
+library(logging)
+
+basicConfig()
+logdebug("1")
+loginfo("2")
+logerror("3")
---
+++ r/logging/3.r
@@ -0,0 +1,6 @@
+library(logging)
+
+basicConfig(level = 30)
+logdebug("1")
+loginfo("2")
+logerror("3")
