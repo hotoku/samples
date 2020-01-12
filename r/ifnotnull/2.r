10d718e9b23458875c653a1c21935992f2a683b8
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-01-12 20:49:11 +0900

    [r] if not null

--- r/ifnotnull/2.r
+++ r/ifnotnull/2.r
@@ -6,6 +6,10 @@ if_not_null <- function(val, default=NULL){
   }
 }
 
+is.rstudio <- function(){
+  commandArgs()[1] == "RStudio"
+}
+
 mystop <- function(msg){
   stop(msg)
 }
@@ -14,10 +18,22 @@ if_not_null(1) %>%
   if_not_null(l1$val) # => 1
 
 if_not_null(1) %>%
-   if_not_null(stop("val is not set")) # 1
+   if_not_null(stop("val is not set")) # => 1
 
 if_not_null(NULL) %>%
-  if_not_null(10)
+  if_not_null(10) # => 10
+
+# if_not_null(NULL) %>%
+#   if_not_null(stop("val is not set")) # error
+
+if_not_null(1) %>%
+  if_not_null(if(is.rstudio()) 10 else stop("val is not set")) # => 1
+
+if_not_null(1) %>%
+  if_not_null(if(!is.rstudio()) 10 else stop("val is not set")) # => 1
 
 if_not_null(NULL) %>%
-  if_not_null(stop("val is not set"))
+  if_not_null(if(is.rstudio()) 10 else stop("val is not set")) # => 10
+
+if_not_null(NULL) %>%
+  if_not_null(if(!is.rstudio()) 10 else stop("val is not set")) # error
