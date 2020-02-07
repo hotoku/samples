034777a24b30fe70c2b5d812d3b3be30070e4d55
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-02-07 19:17:36 +0900

    [bq] bytes

---
+++ bigquery/bytes/3.sql
@@ -0,0 +1,17 @@
+#standardSQL
+
+select
+  from_hex("01") > from_hex("00"),
+  from_hex("01") > from_hex("0000"),
+  from_hex("00") = from_hex("0000"),
+  to_hex(b"1"),
+  cast(to_hex(b"1") as int64)
+
+
+-- +------+------+-------+-----+-----+
+-- | f0_  | f1_  |  f2_  | f3_ | f4_ |
+-- +------+------+-------+-----+-----+
+-- | true | true | false | 31  |  31 |
+-- +------+------+-------+-----+-----+
+
+-- BYTES→INTの変換方法：to_hexでhexiadecimalにして、intにcast
