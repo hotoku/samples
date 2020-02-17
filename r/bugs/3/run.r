8b3a637a1cb0d914e1088715548d50eaeeaed34b
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-02-17 22:14:16 +0900

    [r][bugs] BUGSのサンプル

---
+++ r/bugs/3/run.r
@@ -0,0 +1,28 @@
+library(BRugs)
+library(stringr)
+
+model_file <- "bugs/3/model.txt"
+dat <- list(
+  z = 2,
+  tau = 1e2
+)  
+n_chain = 1
+n_iter = 1e6
+burnin = 0.1
+n_thin = 1e2
+
+modelCheck(model_file)
+data_file <- bugsData(dat, digits = 10)
+modelData(data_file)
+modelCompile(n_chain)
+modelGenInits()
+modelUpdate(n_iter * burnin)
+samplesSet(str_split("x,y", ",")[[1]])
+samplesSetThin(n_thin)  
+modelUpdate(n_iter)
+
+xs <- samplesSample("x")
+ys <- samplesSample("y")
+
+plot(xs, ys)
+cor(xs, ys)
