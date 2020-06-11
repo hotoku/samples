f7b27df6125bf68d0bfa46677f43ae6a5e461e07
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-06-11 11:29:35 +0900

    tiny

--- python/asyncio/10.py
+++ python/asyncio/10.py
@@ -5,29 +5,29 @@ import logging
 from concurrent.futures import ThreadPoolExecutor
 
 
-def f():
+def f(s, t):
     time.sleep(3)
-    return "f"
+    return s + t
 
 
-def g():
+def g(s, t):
     time.sleep(3)
-    return "g"
+    return s + t
 
 
 EX = ThreadPoolExecutor(max_workers=2)
 
 
-async def in_thread(func):
+async def in_thread(func, *args):
     loop = asyncio.get_event_loop()
-    return await loop.run_in_executor(EX, func)
+    return await loop.run_in_executor(EX, func, *args)
 
 
 async def main():
     _t = time.time()
     ret = await asyncio.gather(
-        in_thread(f),
-        in_thread(g)
+        in_thread(f, 1, 2),
+        in_thread(g, 1, 2)
     )
     print(ret)
     print(time.time() - _t)
