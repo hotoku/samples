64a42952726ff08f502684b0c9239865dd850b9f
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-07-12 09:00:36 +0900

    spider

--- python/scrapy/file_download/covid_pdf/spiders/CovidPdf.py
+++ python/scrapy/file_download/covid_pdf/spiders/CovidPdf.py
@@ -4,7 +4,7 @@ import scrapy
 class CovidpdfSpider(scrapy.Spider):
     name = 'CovidPdf'
     allowed_domains = ['www.city.akiruno.tokyo.jp']
-    start_urls = ['http://www.city.akiruno.tokyo.jp/']
+    start_urls = ['http://www.city.akiruno.tokyo.jp/0000010831.html']
 
     def parse(self, response):
-        pass
+        yield dict(file_urls=response.xpath("//li//a@href").re(r".+pdf$"))
