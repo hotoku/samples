617127a1c7ed3bf512d7c25ace9108fbf559ba88
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-07-14 15:58:18 +0900

    [python] mongo

--- python/mongodb/1.py
+++ python/mongodb/1.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 client = MongoClient("localhost", 27017)
 db = client.myNewDatabase
 collection = db.myCollection
-
+collection.delete_many({})
 
 collection.insert_one(dict(
     name="東京スカイツリー",
