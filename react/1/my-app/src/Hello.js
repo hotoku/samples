d2ef8d7bd3603332fc8b0ac86aaaca34adde99bf
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-07-16 18:39:27 +0900

    variable

--- react/1/my-app/src/Hello.js
+++ react/1/my-app/src/Hello.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
 
+const msg = "Hello"
+
 
 function Hello(props) {
-  return <h1>Hello {props.name}!</h1>;
+  return <h1>{msg} {props.name}!</h1>;
 }
 
 
