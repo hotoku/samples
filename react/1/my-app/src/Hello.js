5d35edbcbcf1d7699cde2b74e2c53267bc61951d
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-07-16 19:33:38 +0900

    add user

--- react/1/my-app/src/Hello.js
+++ react/1/my-app/src/Hello.js
@@ -1,21 +1,36 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 class Hello extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { msg: 'Hello!' };
+    this.state = {
+      users: [
+        { name: "Tanaka", age: 26 },
+        { name: "Suzuki", age: 32 },
+      ],
+    };
+  }
+
+  changeState() {
+    let users = this.state.users;
+    users.push({ name: "Yamada", age: 43 });
+    this.setState({ users: users });
   }
 
   render() {
+    let userList = this.state.users.map((user, index) => (
+      <li key={index}>
+        {user.name} (Age: {user.age})
+      </li>
+    ));
     return (
       <div>
-        <h1>{this.state.msg}</h1>
-        <button onClick={() => this.setState({msg: 'Bye!'})}>Click</button>
+        <ul>{userList}</ul>
+        <button onClick={() => this.changeState()}>Click</button>
       </div>
     );
   }
 }
-
 
 export default Hello;
