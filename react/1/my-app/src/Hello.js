43d8027304125d62ab1033610caf13a192a1383b
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-07-16 18:38:34 +0900

    property

---
+++ react/1/my-app/src/Hello.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+
+function Hello(props) {
+  return <h1>Hello {props.name}!</h1>;
+}
+
+
+export default Hello;
--- react/1/my-app/src/index.js
+++ react/1/my-app/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+    <Hello name="World"/>
+    </React.StrictMode>, 
   document.getElementById('root')
 );
 
