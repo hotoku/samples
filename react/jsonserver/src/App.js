44fbf0960ad54c3a4c98dbdd91393ab55e73590a
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-07-20 20:36:50 +0900

    auto update

--- react/jsonserver/src/App.js
+++ react/jsonserver/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+class NameComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: props.value };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    const updateValue = () => {
+      const v = this.state;
+      v.value = this.state.value + "x";
+      this.setState(v);
+      setTimeout(updateValue, 1000);
+    };
+    setTimeout(updateValue, 1000);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+      </form>
+    );
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <NameComponent value="horikoshi" />;
 }
 
 export default App;
