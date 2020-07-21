71469438f37cd5928a743e1b6d35d89a5f0a45c1
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-07-22 08:34:34 +0900

    leaflet

--- react/leaflet/src/index.js
+++ react/leaflet/src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const position = [51.505, -0.09];
+const map = (
+  <Map center={position} zoom={13}>
+    <TileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    />
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup.
+        <br />
+        Easily customizable.
+      </Popup>
+    </Marker>
+  </Map>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(map, document.getElementById("map-container"));
