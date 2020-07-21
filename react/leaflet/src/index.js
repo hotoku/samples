dae149aaeead7cd80ec16f51cb747683820907aa
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-07-22 08:57:52 +0900

    wip

--- react/leaflet/src/index.js
+++ react/leaflet/src/index.js
@@ -1,22 +1,33 @@
 import React from "react";
-import { render } from "react-dom";
-import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import ReactDOM from "react-dom";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
-const position = [51.505, -0.09];
-const map = (
-  <Map center={position} zoom={13}>
-    <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    />
-    <Marker position={position}>
-      <Popup>
-        A pretty CSS3 popup.
-        <br />
-        Easily customizable.
-      </Popup>
-    </Marker>
-  </Map>
-);
+class SimpleExample extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      lat: 51.505,
+      lng: -0.09,
+      zoom: 13,
+    };
+  }
 
-render(map, document.getElementById("map-container"));
+  render() {
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <Map center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+    );
+  }
+}
+
+ReactDOM.render(<SimpleExample />, document.getElementById("map-container"));
