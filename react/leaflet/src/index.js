095a8693c640f042c309cf61b157681877fed47f
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-07-25 21:34:43 +0900

    [react] leaflet polygon

--- react/leaflet/src/index.js
+++ react/leaflet/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import { Map, TileLayer, Marker, Popup, Polygon } from "react-leaflet";
 import "./index.css";
 
 class SimpleExample extends React.Component {
@@ -31,4 +31,49 @@ class SimpleExample extends React.Component {
   }
 }
 
-ReactDOM.render(<SimpleExample />, document.getElementById("map-container"));
+class PolygonExample extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      viewport: {
+        center: [51.505, -0.09],
+        zoom: 13,
+      },
+    };
+  }
+  render() {
+    const pos = [
+      [51.505, -0.085],
+      [51.51, -0.085],
+      [51.51, -0.09],
+      [51.505, -0.09],
+    ];
+
+    return (
+      <div>
+        <Map viewport={this.state.viewport}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+          />
+          <Polygon color="red" positions={pos} />
+        </Map>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>SimpleExample</h1>
+        <SimpleExample />
+        <h1>PolygonExample</h1>
+        <PolygonExample />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("map-container"));
