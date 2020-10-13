f738211ffa8be5351e3ff5e4d0daca4177508a53
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-10-14 08:39:54 +0900

    [cpp]

---
+++ cpp/max.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+#define TYPEMAX(type) {                                                 \
+    const unsigned long long temp = (1llu << (sizeof(type)*8 - 1)) - 1; \
+    cout << temp << endl;                                               \
+  }
+
+#define UTYPEMAX(type) {                                                \
+    const unsigned long long temp1 = (1llu << (sizeof(type)*8 - 1)) - 1; \
+    const unsigned long long temp2 = (1llu << (sizeof(type)*8 - 1));    \
+    cout << temp1+temp2 << endl;                                        \
+  }
+
+
+int main(){
+  TYPEMAX(char);
+  TYPEMAX(int);
+  TYPEMAX(long long);
+  UTYPEMAX(unsigned char);
+  UTYPEMAX(unsigned int);
+  UTYPEMAX(unsigned long long);
+}
