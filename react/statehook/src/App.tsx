a31b78d20417d1cd9adc663ccd30093219a9c4eb
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-10-30 20:42:22 +0900

    [react] state hook

---
+++ react/statehook/src/App.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <p>
+            count = {count}
+          </p>
+          <button onClick={() => setCount(count + 1)}>click</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
