d7fc2665cfb91e2e367d1a5fcf292d70626a688a
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-10-30 23:41:07 +0900

    [react] count

--- react/statehook/src/App.tsx
+++ react/statehook/src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-  const [nRender, setNRender] = useState(0);
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
+
   return (
     <div className="App">
       <header className="App-header">
