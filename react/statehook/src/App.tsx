5617776ab7a8e9f5c8c51b36ca08be605996be72
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-10-30 21:41:00 +0900

    wip

--- react/statehook/src/App.tsx
+++ react/statehook/src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [nRender, setNRender] = useState(0);
   const [count, setCount] = useState(0);
   return (
     <div className="App">
