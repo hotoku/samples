1233a5413f1ff9c8e4fe17fe9c82fab5318d85dd
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-11-05 08:50:00 +0900

    route

--- react/typescript2/src/App.tsx
+++ react/typescript2/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Sample1 } from "./components/sample1";
 import { Sample2 } from "./components/sample2";
 import { Sample3 } from "./components/sample3";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 
 const Line = () => {
   return (
@@ -19,11 +20,26 @@ const Line = () => {
 function App() {
   return (
     <div>
-      <Sample1 name="sample1" />
-      <Line />
-      <Sample2 cnt={0} />
-      <Line />
-      <Sample3 />
+      <BrowserRouter>
+        <ul>
+          <li>
+            <Link to="/1">1</Link>
+          </li>
+          <li>
+            <Link to="/2">2</Link>
+          </li>
+          <li>
+            <Link to="/3">3</Link>
+          </li>
+        </ul>
+        <Line />
+
+        <Switch>
+          <Route exact path="/1" render={() => <Sample1 name={"sample1"} />} />
+          <Route exact path="/2" render={() => <Sample2 cnt={0} />} />
+          <Route exact path="/3" component={Sample3} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
