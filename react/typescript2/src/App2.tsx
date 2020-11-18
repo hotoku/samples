888b2d33ed4d54da689289c935b7598c461231cc
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-11-18 22:27:59 +0900

    tiny

--- react/typescript2/src/App2.tsx
+++ react/typescript2/src/App2.tsx
@@ -5,7 +5,9 @@ function App() {
   const [counter, setCounter] = useState(0);
   return (
     <div>
-      <button onClick={() => setCounter(counter + 1)}>click</button>
+      <p>
+        <button onClick={() => setCounter(counter + 1)}>click</button>
+      </p>
       <p>count={counter}</p>
     </div>
   );
