c0ced7c5f9014dfb0b3a7a25e39e2d9eee27d0cc
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-11-19 19:59:17 +0900

    wip works.

--- react/typescript2/src/components/sample11.tsx
+++ react/typescript2/src/components/sample11.tsx
@@ -1,27 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 
-interface Props {}
+type FontContextValue = {
+  size: number;
+  setSize: (size: number) => void;
+};
 
-export const FontContext = React.createContext([0, (x: number) => {}]);
+const defaultFontContextValue: FontContextValue = {
+  size: 16,
+  setSize: (size: number) => {},
+};
 
-export const Sample11 = (props: Props) => {
+export const FontContext = React.createContext(defaultFontContextValue);
+
+const Text = (props: {}) => {
+  const { size, setSize } = useContext(FontContext);
+  return (
+    <div>
+      <button onClick={() => setSize(size + 4)}>+4</button>
+    </div>
+  );
+};
+
+export const Sample11 = (props: {}) => {
   const [size, setSize] = useState(16);
-
-  const _setSize = (size: number) => {
-    setSize(size);
-  };
 
   return (
     <div>
-      <FontContext.Provider value={[size, _setSize]}>
-        <h1 style={{ fontSize: `${size}px` }}>FONT SIZE {size}px</h1>
+      <FontContext.Provider value={{ size, setSize }}>
         <form onSubmit={(e) => e.preventDefault()}>
+          <Text />
           <input
             type="number"
             value={size}
             onChange={(e) => setSize(parseFloat(e.target.value))}
           />
         </form>
+        <h1 style={{ fontSize: `${size}px` }}>FONT SIZE {size}px</h1>
       </FontContext.Provider>
     </div>
   );
