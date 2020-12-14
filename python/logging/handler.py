f6255bb96eaaa8479f6ee0b6827500aece076dc2
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-12-14 15:50:04 +0900

    formatter

--- python/logging/handler.py
+++ python/logging/handler.py
@@ -4,15 +4,20 @@ import re
 
 LOGGER = logging.getLogger(__file__)
 
-_to_stderr = logging.StreamHandler(sys.stderr)
-_to_stderr.setLevel(logging.WARNING)
+_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+_to_stderr = logging.StreamHandler(sys.stderr)
 _to_file = logging.FileHandler(re.sub(".py$", ".log", __file__))
-_to_file.setLevel(logging.INFO)
+_to_stderr.setFormatter(_formatter)
+_to_file.setFormatter(_formatter)
 
 LOGGER.addHandler(_to_stderr)
 LOGGER.addHandler(_to_file)
+
 LOGGER.setLevel(logging.INFO)
+_to_stderr.setLevel(logging.WARNING)
+_to_file.setLevel(logging.INFO)
+
 
 LOGGER.debug("debug")
 LOGGER.info("info")
