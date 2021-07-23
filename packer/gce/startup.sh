ddf9881db30a7fcc4da05ecdeb9628527a5b6b47
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-07-23 13:20:16 +0900

    [packer] openjdk-8-jdk

--- packer/gce/startup.sh
+++ packer/gce/startup.sh
@@ -10,7 +10,7 @@ apt install -y python3 python3-pip emacs
 
 ## bazel
 # cf: https://docs.bazel.build/versions/1.0.0/install-ubuntu.html
-apt install -y pkg-config zip g++ zlib1g-dev unzip python3 
+apt install -y pkg-config zip g++ zlib1g-dev unzip python3 openjdk-8-jdk
 wget https://github.com/bazelbuild/bazel/releases/download/1.0.0/bazel-1.0.0-installer-linux-x86_64.sh
 chmod u+x bazel-1.0.0-installer-linux-x86_64.sh
 ./bazel-1.0.0-installer-linux-x86_64.sh 2>&1 | tee /hotoku/bazel.log
