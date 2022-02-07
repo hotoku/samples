311bdb3147e2c514aa10f6c5096d4acd85148711
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-02-07 16:18:39 +0900

    [pandoc] revealjs filter

--- pandoc/revealjs/filter.py
+++ pandoc/revealjs/filter.py
@@ -27,14 +27,35 @@ def main():
     buf = Buffer()
     for l in sys.stdin.readlines():
         buf.append(l)
-        print(l, end="")
         if len(buf) < 2:
+            print(l, end="")
             continue
-        if buf[1].strip() == '<script src="https://unpkg.com/reveal.js@^4//plugin/math/math.js"></script>':
+        elif buf[1].strip() == '<script src="https://unpkg.com/reveal.js@^4//plugin/math/math.js"></script>':
+            print(l, end="")
             print('<script src="plugin/menu/menu.js"></script>')
-        if buf[0].strip() == '// reveal.js plugins' and \
-           buf[1].strip() == 'plugins: [':
+        elif buf[0].strip() == '// reveal.js plugins' and \
+                buf[1].strip() == 'plugins: [':
+            print(l, end="")
             print("RevealMenu,")
+        elif buf[1].strip() == '<link rel="stylesheet" href="https://unpkg.com/reveal.js@^4//dist/theme/black.css" id="theme">':
+            print("""
+<link rel="stylesheet" href="https://unpkg.com/reveal.js@^4//dist/theme/white.css" id="theme">
+<style>
+  ul li {
+      padding-bottom:0.4em;
+  }
+  ol li {
+      padding-bottom:0.4em;
+  }
+  :root {
+      --r-heading1-size: 1.2em;
+      --r-heading-text-transform: none;
+      --r-main-font-size: 34px;
+  }
+</style>
+""")
+        else:
+            print(l, end="")
 
 
 if __name__ == "__main__":
