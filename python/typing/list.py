e50de65adefc5f5b0d5321269adb0aceb293cd07
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-02-22 17:01:47 +0900

    [python] typing. comment

--- python/typing/list.py
+++ python/typing/list.py
@@ -1,4 +1,7 @@
 # Listの値の型は、invariantという例
+# https://stackoverflow.com/questions/64481378/expected-type-lista-matched-generic-type-list-t-got-listb-instead
+# > A mutable container such as List is invariant because elements can be both inserted into (contravariant) and
+# > taken from (covariant) the list. If mutability is not required, using an immutable Sequence provides valid type annotation:
 
 from typing import List
 
