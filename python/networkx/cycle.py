e411adf968aafc59e8c2f17ce04ae469c4a406b2
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-05-11 15:54:35 +0900

    [python/networkx] cycle

--- python/networkx/cycle.py
+++ python/networkx/cycle.py
@@ -12,3 +12,18 @@ except nx.exception.NetworkXNoCycle:
 g.add_edge(3, 1)
 ret = nx.find_cycle(g)
 print(ret)
+
+g2 = nx.DiGraph()
+g2.add_edge(1, 2)
+g2.add_edge(1, 3)
+g2.add_edge(2, 3)
+try:
+    # デフォルトでは、エッジの向きを気にする
+    nx.find_cycle(g2)
+except nx.exception.NetworkXNoCycle:
+    pass
+
+
+# orientationに"ignore"を指定すると、向きを無視する
+ret = nx.find_cycle(g2, None, "ignore")
+print(ret)
