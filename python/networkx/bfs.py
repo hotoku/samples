4fd02f3e7528aee872556c6acf2583a8137a5948
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-05-24 04:11:40 +0900

    [python/networkx] bfs

--- python/networkx/bfs.py
+++ python/networkx/bfs.py
@@ -8,6 +8,7 @@ g.add_edge(11, 22)
 g.add_edge(12, 23)
 g.add_edge(12, 24)
 
+
 print(list(nx.bfs_successors(g, 1)))
 # => [(1, [11, 12]), (11, [21, 22]), (12, [23, 24])]
 
@@ -20,3 +21,6 @@ for e in nx.edge_bfs(g, 1):
 # (11, 22)
 # (12, 23)
 # (12, 24)
+
+for e in nx.edge_bfs(g, 24, "reverse"):
+    print(e)
