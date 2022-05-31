0324d1d4c4d9c7a0441d6e153a00241e6526d56b
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-05-31 16:22:34 +0900

    setitem

--- python/setitem/1.py
+++ python/setitem/1.py
@@ -12,3 +12,24 @@ class A:
 a = A()
 a["a"] = 1
 print(a["a"])
+
+
+class B:
+    def __init__(self):
+        self.array = [
+            [0 for _ in range(10)]
+            for _ in range(10)
+        ]
+
+    def __getitem__(self, pair):
+        r, c = pair
+        return self.array[r][c]
+
+    def __setitem__(self, pair, v):
+        r, c = pair
+        self.array[r][c] = v
+
+
+b = B()
+b[3, 3] = 100
+print(b.array)
