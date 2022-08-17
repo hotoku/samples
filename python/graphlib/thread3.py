07815dcba9bbf2fe4eadfefe3e7eb17a102b6023
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-08-17 11:38:17 +0900

    [python] graphlib

---
+++ python/graphlib/thread3.py
@@ -0,0 +1,58 @@
+# thread.pyの実装で、並列な処理になってるのか？
+# やっぱなってないな
+
+# A Python example doing parallel sorting
+# of the nodes of a DAG in topological order
+
+import threading
+import queue
+from graphlib import TopologicalSorter
+import time
+from datetime import datetime
+
+# The queues
+workQueue = queue.Queue()
+doneQueue = queue.Queue()
+
+# Worker thread function
+
+
+def worker():
+    while True:
+        workPiece = workQueue.get()
+        print(datetime.now(), f"start {workPiece}")
+        n = 0
+        while n < 1e8:
+            n += 1
+        print(datetime.now(), f"end {workPiece}")
+        workQueue.task_done()
+        doneQueue.put(workPiece)
+
+
+# Create and start the worker thread
+threading.Thread(target=worker, daemon=True).start()
+
+# Define a workflow as a DAG using a Python dictionary
+workflow = {
+    4: [1],
+    5: [2],
+    6: [3]
+}
+topologicalSorter = TopologicalSorter(workflow)
+topologicalSorter.prepare()
+
+# Loop till the workflow is alive
+while topologicalSorter.is_active():
+    # Get a piece of work that is ready for execution
+    for workPiece in topologicalSorter.get_ready():
+        workQueue.put(workPiece)
+
+    # Mark the completed workpiece as done
+    node = doneQueue.get()
+    topologicalSorter.done(node)
+    print(node)
+
+# Wait for the worker thread to complete
+workQueue.join()
+
+print(topologicalSorter.is_active())
