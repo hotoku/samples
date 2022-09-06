6c69240f8c9fd81151c2133989ef1c9b056a1da7
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-09-07 08:17:37 +0900

    [python] logging basic config

--- python/logging/basic_config.py
+++ python/logging/basic_config.py
@@ -4,7 +4,7 @@ LOGGER = logging.getLogger(__name__)
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s - (pid: %(process)d) - %(threadName)s"
 )
 
 LOGGER.debug("this is debug")
