56476fed2789013e92fd1c6cacf5e2d9b79b81c3
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-09-07 13:25:05 +0900

    [python] boto

---
+++ python/boto/s3.py
@@ -0,0 +1,14 @@
+import boto3
+from boto3.session import Session
+
+profile_name = "sciseed"
+bucket_name = "sagemaker-ap-northeast-1-711091934628"
+
+session = Session(profile_name=profile_name) # ~/.aws/credentialsのプロファイル名
+s3 = session.resource("s3")
+
+bucket = s3.Bucket(bucket_name)
+objs = bucket.objects.all()
+
+for obj in objs:
+    print(obj.key)
