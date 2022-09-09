b0693adc710bde60100fb6f5833cdb523fb3df36
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-09-09 10:48:55 +0900

    [emacs]

---
+++ emacs/.dir-locals.el
@@ -0,0 +1,8 @@
+(
+ (nil . ((sample-value-0 . "sample-0")))
+ (text-mode . ((sample-value-1 . "sample-1")))
+ ("./local" . ((sample-value-2 . "sample-2")))
+)
+
+;; list[tuple[モード名, list[tuple[変数名, 値]]]]という型
+;; ネストが面倒なので注意
