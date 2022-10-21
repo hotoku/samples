f07461b8a22365d5bf4b13170c55c687f8f95556
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-21 16:11:06 +0900

    [sqlite] bool

--- sqlite/bool/run.sh
+++ sqlite/bool/run.sh
@@ -7,16 +7,36 @@ create table bools (
        name text not null,
        bool interger nullable
 );
-insert into bools (name, bool) values ("TRUE", 1);
-insert into bools (name, bool) values ("FALSE", 0);
+insert into bools (name, bool) values ("1", 1);
+insert into bools (name, bool) values ("0", 0);
 insert into bools (name, bool) values ("NULL", NULL);
+insert into bools (name, bool) values ("TRUE", TRUE);
+insert into bools (name, bool) values ("FALSE", FALSE);
 EOF
 fi
 
 
+echo "--"
 echo "select * from bools where bool" | sqlite3 db.sqlite
-# => TRUE|1
 
+echo "--"
+echo "select * from bools where not bool" | sqlite3 db.sqlite
 
-echo "select count(1) from bools where bool" | sqlite3 db.sqlite
-# => 1
+echo "--"
+echo "select * from bools where bool = TRUE" | sqlite3 db.sqlite
+
+echo "--"
+echo "select * from bools where bool = FALSE" | sqlite3 db.sqlite
+
+# --
+# 1|1
+# TRUE|1
+# --
+# 0|0
+# FALSE|0
+# --
+# 1|1
+# TRUE|1
+# --
+# 0|0
+# FALSE|0
