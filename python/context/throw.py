c836c74475068fe4d262c3bc88d3691b26e09d88
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-11-10 10:48:57 +0900

    [python] context manager

--- python/context/throw.py
+++ python/context/throw.py
@@ -5,11 +5,14 @@ class MyContext:
     def __enter__(self):
         print("enter")
 
-    def __exit__(self, exc, *_):
-        print(f"exit: {exc}")
+    def __exit__(self, *args, **kw_args):
+        print(f"exit: {args}")
+        print(f"exit: {kw_args}")
 
 
 try:
+    with MyContext():
+        pass
     with MyContext():
         raise Exception("my exception")
 except Exception as e:
