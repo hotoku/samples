8f4847546e0b5134e6a573b916125df12962b53b
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-11-12 08:25:48 +0900

    [graphql] tutorial

--- graphql/src/index.ts
+++ graphql/src/index.ts
@@ -12,13 +12,21 @@ app.use(morgan("combined"));
 
 const schema = buildSchema(`
   type Query {
-    hello: String
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
   }
 `);
 
 const root = {
-  hello: () => {
-    return "Hello World";
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? "Take it easy" : "Salvation lies within";
+  },
+  random: () => {
+    return Math.random();
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map((_) => 1 + Math.floor(Math.random() * 6));
   },
 };
 
