7e09132f9c61ede622c933a9b754a03b46e80721
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-11-12 08:17:25 +0900

    [graphql] hello

--- graphql/src/index.ts
+++ graphql/src/index.ts
@@ -2,16 +2,38 @@ import express from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import * as portfinder from "portfinder";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(morgan("combined"));
 
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => {
+    return "Hello World";
+  },
+};
+
 (async () => {
   const port = await portfinder.getPortPromise({
     port: 3000,
   });
   console.log("port =", port);
+  app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema: schema,
+      rootValue: root,
+      graphiql: true,
+    })
+  );
   app.listen(port);
 })();
