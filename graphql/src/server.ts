6c628e044e2a0faea6a5556f5c51c3aa559d8876
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-11-14 20:37:22 +0900

    [gql] done

--- graphql/src/server.ts
+++ graphql/src/server.ts
@@ -5,16 +5,48 @@ import bodyParser from "body-parser";
 import morgan from "morgan";
 import { getInstance } from "./db";
 
-function getUser(args: { id: number }): {
+type TeamResolver = {
   id: number;
   name: () => Promise<string>;
-} {
+  users: () => Promise<UserResolver[]>;
+};
+
+type UserResolver = {
+  id: number;
+  name: () => Promise<string>;
+  team: () => Promise<TeamResolver>;
+};
+
+function getUser(args: { id: number }): UserResolver {
   return {
     id: args.id,
     name: async () => {
       const db = await getInstance();
-      const ret = await db.all("select * from users where id=?", args.id);
+      const ret = await db.all("select name from users where id=?", args.id);
       return ret[0].name;
+    },
+    team: async () => {
+      const db = await getInstance();
+      const ret = await db.all("select teamId from users where id=?", args.id);
+      const teamId = ret[0].teamId;
+      return getTeam({ id: teamId });
+    },
+  };
+}
+
+function getTeam(args: { id: number }): TeamResolver {
+  return {
+    id: args.id,
+    name: async () => {
+      const db = await getInstance();
+      const ret = await db.all("select name from teams where id=?", args.id);
+      return ret[0].name;
+    },
+    users: async () => {
+      const db = await getInstance();
+      const ret = await db.all("select id from users where teamId=?", args.id);
+      console.log(ret);
+      return ret.map((x) => getUser({ id: x.id }));
     },
   };
 }
@@ -26,6 +58,12 @@ function createApp(): express.Express {
     type User {
       id: Int
       name: String
+      team: Team
+    }
+    type Team {
+      id: Int
+      name: String
+      users: [User]
     }
     type Query {
       getUser(id: Int!): User
