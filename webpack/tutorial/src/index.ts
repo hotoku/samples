c4e5be6c841ed52e55538db597325d8df8954fbf
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-11-16 17:56:06 +0900

    [webpack] split output

--- webpack/tutorial/src/index.ts
+++ webpack/tutorial/src/index.ts
@@ -5,11 +5,19 @@ import Data from "./data.xml";
 // @ts-ignore
 import Notes from "./data.csv";
 
+import printMe from "./print";
+
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement("button");
 
   element.innerHTML = _.join(["Hello", "webpack", "!"], " ");
   element.classList.add("hello");
+
+  btn.innerHTML = "Click";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   console.log(Data);
   console.log(Notes);
--- webpack/tutorial/webpack.config.js
+++ webpack/tutorial/webpack.config.js
@@ -1,7 +1,10 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.ts",
+  entry: {
+    index: "./src/index.ts",
+    print: "./src/print.ts",
+  },
   module: {
     rules: [
       {
@@ -31,7 +34,7 @@ module.exports = {
     extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
 };
