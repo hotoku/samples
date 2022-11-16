eabf0bff7c281933e4bf99d526651afce31c02ca
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-11-16 13:29:57 +0900

    [webpack] asset management

--- webpack/tutorial/webpack.config.js
+++ webpack/tutorial/webpack.config.js
@@ -1,22 +1,25 @@
-const path = require('path');
-
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
+      },
+      {
+        test: /.css$/i,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
